0f9a7ffda12410048f90486879b8b20ade70ec9c
Edem Kumodzi <edem@kumodzi.com>
2013-10-05 16:50:58 +0000

    Push to heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,13 @@ class HomeController < ApplicationController
 
   def index
   	@campaigns = Campaign.where(:status => false)
+
+  	respond_to do |format|
+    	format.html # index.html.erb
+    	format.json  { render :json => @campaigns }
   end
+
+  end
+
+
 end
