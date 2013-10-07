56e6cdef3e0f446f7e17b6aed5740177db3a90c4
Edem Kumodzi <edem@kumodzi.com>
2013-10-07 17:00:44 +0000

    Clean

--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -1,70 +1,47 @@
 class CampaignController < ApplicationController
   def view
-  	@campaign = Campaign.find(params[:id])
+    @campaign = Campaign.find(params[:id])
 
-  	@donation = Donation.new
+    @donation = Donation.new
   end
 
   def request_checkout
-  	@donation = Donation.new(params[:donation])
+    @donation = Donation.new(params[:donation])
 
-  	@donation.campaign = Campaign.find(params[:id])
+    @donation.campaign = Campaign.find(params[:id])
 
-  	if @donation.valid?
+    if @donation.valid?
 
-  		invoice = MPower::Checkout::Invoice.new
+      invoice = MPower::Checkout::Invoice.new
+      invoice.add_item(@donation.campaign.title,1,@donation.amount,@donation.amount);
+      invoice.description = "Donation to support " + @donation.campaign.title;
+      invoice.total_amount = @donation.amount.to_f
+      invoice.add_custom_data("CampaignID",@donation.campaign.id);
+      invoice.return_url = confirm_url(@campaign)
+      invoice.create
+      redirect_to invoice.invoice_url
 
-  		invoice.add_item(@donation.campaign.title,1,@donation.amount,@donation.amount);
-
-  		invoice.description = "Donation to support " + @donation.campaign.title;
-
-  		invoice.total_amount = @donation.amount.to_f
-
-  		invoice.add_custom_data("CampaignID",@donation.campaign.id);
-
-  		invoice.return_url = confirm_url(@campaign)
-
-  		if invoice.create
-  			#puts invoice.status
-  			#puts invoice.response_text
-  			# you may want to do "redirect_to invoice.invoice_url" in a Rails controllerwwwwww
-  			redirect_to invoice.invoice_url
-		else
-  			#puts invoice.status
-  			#invoice.response_text
-		end
-
-
-  	else
-  		render 'view'
-  	end
+    else
+      render 'view'
+    end
   end
 
   def confirm
-  	token = params[:token]
+    token = params[:token]
 
-  	invoice = MPower::Checkout::Invoice.new
-	if invoice.confirm(token)
+    invoice = MPower::Checkout::Invoice.new
+    if invoice.confirm(token)
 
-		@donation = Donation.new
-		@donation.amount = invoice.total_amount
-		@donation.name = invoice.get_customer_info "name"
-		@donation.mpower_email = invoice.get_customer_info "email"
-		@donation.mpower_phone = invoice.get_customer_info "phone"
-		@donation.campaign = Campaign.find(invoice.get_custom_data "CampaignID")
-
-
-		@donation.save
-
-		flash[:notice] = "Thank you, " + @donation.name + " for supporting " + @donation.campaign.title + " with " + @donation.amount.to_f.to_s + " GHS!"
-
-		redirect_to root_url
-
-		else
-  			puts invoice.status
-  			puts invoice.response_text
-  			puts invoice.response_code
-  		end
+      @donation = Donation.new
+      @donation.amount = invoice.total_amount
+      @donation.name = invoice.get_customer_info "name"
+      @donation.mpower_email = invoice.get_customer_info "email"
+      @donation.mpower_phone = invoice.get_customer_info "phone"
+      @donation.campaign = Campaign.find(invoice.get_custom_data "CampaignID")
+      @donation.save
+      flash[:notice] = "Thank you, " + @donation.name + " for supporting " + @donation.campaign.title + " with " + @donation.amount.to_f.to_s + " GHS!"
+      redirect_to root_url
+    end
 
   end
 end
